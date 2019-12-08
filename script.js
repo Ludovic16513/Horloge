7ce8e7adbd907555218cfc886f7c52faf644d089
Ludovic16513 <56023526+Ludovic16513@users.noreply.github.com>
2019-12-08 23:37:35 +0100

    CSS a travailler

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
+var now = new Date;
+var seconde = now.getSeconds();
+var minute = now.getMinutes();
+var heure = now.getHours();
 
-function rotate()
-{
-    setTimeout(rotate,1000);
+var aiguilleSeconde = document.getElementById("secondes");
+var aiguilleMinute = document.getElementById("minute");
+var aiguilleHeure = document.getElementById("heure");
 
+aiguilleSeconde.style.transform = "rotate(" + (seconde*6) + "deg)";
+aiguilleMinute.style.transform = "rotate(" + (minute*6) +"deg)";
+aiguilleHeure.style.transform = "rotate(" + (heure*30) +"deg)";
+
+
+function horloge(){
+
+    setTimeout(horloge,1000);
+// temps
     var now = new Date;
     var seconde = now.getSeconds();
     var minute = now.getMinutes();
     var heure = now.getHours();
-
+//ID
     var aiguilleSeconde = document.getElementById("secondes");
     var aiguilleMinute = document.getElementById("minute");
     var aiguilleHeure = document.getElementById("heure");
 
 
+    //Rotation
     aiguilleSeconde.style.transform = "rotate(" + (seconde*6) + "deg)";
-    aiguilleMinute.style.transform = "rotate(" + (minute*6) + "deg)";
-    aiguilleHeure.style.transform = "rotate(" + (minute*30) + "deg)";
-    console.log(seconde);
 
-    if (seconde > 59){
+    if (seconde === 0){
         aiguilleMinute.style.transform = "rotate(" + (minute*6) + "deg)";
     }
 
-    if (minute > 59){
+    if(minute === 0){
         aiguilleHeure.style.transform = "rotate(" + (heure*30) + "deg)";
     }
 
-    if (heure > 23){
-        aiguilleHeure.style.transform = "rotate(" + (heure*30) + "deg)";
-    }
+    console.log(seconde);
 }
-rotate();
+
+horloge();
 
